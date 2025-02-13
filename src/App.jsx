35bd6880320932ced9f2ee07250b482a5ce71b9a
EliseAur <94222653+EliseAur@users.noreply.github.com>
2025-02-13 18:08:11 +0100

    Working on styles for the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -10,31 +10,37 @@ function App() {
     <div id="" className="">
       <div
         className="headerNavBox bg-cover bg-bottom
-       bg-no-repeat h-screen relative"
+       bg-no-repeat bg-fixed h-screen relative"
       >
         <div className="absolute inset-0 bg-black opacity-40"></div>
-        <header className="text-beige font-bold text-shadow w-full relative z-10">
-          <div className="container mx-auto flex justify-between items-center p-4">
-            <div className="text-lg font-bold">
+        <header className="text-beige font-bold w-full relative z-10">
+          <div className="container mx-auto flex justify-between items-center p-3">
+            <div>
               <a
                 href="#"
-                className="font-black italic hover:text-gray-200 text-2xl sm:text-3xl"
+                className="text-lg text-shadow font-black italic text-lightGreen hover:text-darkGreen text-2xl sm:text-3xl"
               >
                 HoliDaze
               </a>
             </div>
-            <nav className="space-x-4">
-              <a href="#" className="hover:text-gray-200">
+            <nav className="hidden md:flex md:items-center -ml-16">
+              <a
+                href="#"
+                className="text-shadow hover:border hover:border-beige rounded-sm px-3 py-1 x-3 underline decoration-2"
+              >
                 Home
               </a>
-              <a href="#" className="hover:text-gray-200">
-                About
+              <a
+                href="#"
+                className="text-shadow hover:border hover:border-beige rounded-sm px-3 py-1 mx-3"
+              >
+                All venues
               </a>
-              <a href="#" className="hover:text-gray-200">
-                Contact
-              </a>
-              <a href="#" className="text-shadow hover:text-gray-200">
-                Cart
+              <a
+                href="#"
+                className="text-shadow hover:border hover:border-beige rounded-sm px-3 py-1 mx-3"
+              >
+                Become a host
               </a>
             </nav>
             <div>Login</div>
@@ -42,10 +48,10 @@ function App() {
         </header>
         <hero className="text-beige relative z-10 text-center">
           <div className="py-20 px-4 sm:px-6 lg:px-8">
-            <h1 className="text-4xl sm:text-5xl lg:text-6xl font-black italic text-shadow">
+            <h1 className="text-shadow text-4xl sm:text-5xl lg:text-6xl font-black italic ">
               Find Your Perfect Getaway
             </h1>
-            <div className="font-bold mt-4 text-lg sm:text-xl lg:text-2xl max-w-2xl mx-auto text-shadow">
+            <div className="font-bold text-shadow mt-4 text-lg sm:text-xl lg:text-2xl max-w-2xl mx-auto">
               <p>Book unique stays in stunning destinations</p>
               <p>
                 Or share your home with the world by register as a host and earn
@@ -54,18 +60,21 @@ function App() {
             </div>
             <div className="max-w-[300px] mx-auto">
               <a
-                className="mt-8 inline-block bg-lightGreen text-black font-bold px-6 py-3 rounded-md text-lg hover:bg-zinc-800 w-full"
+                className="w-full mt-8 inline-block bg-lightGreen text-black font-bold px-6 py-2 rounded-sm text-lg hover:bg-darkGreen"
                 href="/"
               >
                 Latest venues
               </a>
-              <div>
+              <div className="flex mt-4 w-full">
                 <input
                   type="text"
                   placeholder="Search for venues..."
-                  className="w-full mt-4 py-3 px-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-zinc-900"
+                  className="flex-grow w-full sm:w-auto py-2 px-3 rounded-l-sm focus:outline-none input-inner-shadow bg-zinc-800/50 bg-opacity-90 border border-beige border-2 text-beige placeholder-stone-400"
                   value=""
-                ></input>
+                />
+                <button className="bg-beige text-black font-bold px-3 py-2 rounded-r-sm hover:bg-darkBeige">
+                  Search
+                </button>
               </div>
             </div>
           </div>
