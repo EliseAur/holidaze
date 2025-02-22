3592b214d6d04171de702efdbf6bc5e8ee7eed24
EliseAur <94222653+EliseAur@users.noreply.github.com>
2025-02-22 12:37:24 +0100

    styling changes to login form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 export default function Login() {
   return (
-    <div className="w-full max-w-md p-4 mx-auto">
-      <h1 className="text-3xl font-black italic text-lightGreen text-shadow mb-4">
+    <div className="w-full max-w-xs p-4 mx-auto">
+      <h1 className="text-4xl font-black italic text-lightGreen text-shadow mb-4">
         Login
       </h1>
       <form className="">
@@ -18,7 +18,7 @@ export default function Login() {
               type="email"
               placeholder="name@stud.noroff.no"
               // {...register("email")}
-              className="p-2 border border-darkBeige bg-lightBeige rounded-sm shadow-custom-dark w-full focus:border-lightGreen focus:border-2 focus:ring-lightGreen focus:outline-none"
+              className="px-2 py-1 border border-darkBeige bg-lightBeige rounded-sm shadow-custom-dark w-full focus:border-lightGreen focus:border-2 focus:ring-lightGreen focus:outline-none"
             />
             {/* <p className="text-red-600 text-sm">{errors.email?.message}</p> */}
           </div>
@@ -36,7 +36,7 @@ export default function Login() {
               // onChange={(e) => setPassword(e.target.value)}
               placeholder="Enter password"
               // {...register("subject")}
-              className="p-2 border border-darkBeige bg-lightBeige rounded-sm shadow-custom-dark w-full focus:border-lightGreen focus:border-2 focus:ring-lightGreen focus:outline-none"
+              className="px-2 py-1 border border-darkBeige bg-lightBeige rounded-sm shadow-custom-dark w-full focus:border-lightGreen focus:border-2 focus:ring-lightGreen focus:outline-none"
             />
             {/* <p className="text-red-600 text-sm">{errors.subject?.message}</p> */}
           </div>
