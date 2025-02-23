77e1e7b7862cbc2408b65ce59471881b8b683e53
EliseAur <94222653+EliseAur@users.noreply.github.com>
2025-02-23 11:54:21 +0100

    Fixing the VenueCard to get all the data/values from API on it, and adding correct linking to VenueDetail page

--- src/components/VenueCard.jsx
+++ src/components/VenueCard.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faWifi,
@@ -12,67 +13,84 @@ import {
 export default function VenueCard({ venue }) {
   return (
     <div className="bg-lightBeige rounded-sm shadow-lg relative">
-      <a href="">
-        {/* <img
-        src={}
-          src="/src/images/home-beach.jpg"
-          alt="Beautiful home at the beach"
-          className="rounded-t-sm"
-        /> */}
+      <Link to={`/venue/${venue.id}`}>
         <img
-          src={venue.media[0].url}
-          alt={venue.media[0]?.alt}
-          className="rounded-t-sm"
+          src={venue.media[0]?.url || "src/images/home-beach.jpg"}
+          alt={venue.media[0]?.alt || "Beautiful home at the beach"}
+          className="rounded-t-sm w-full h-56 object-cover object-center"
         />
-      </a>
+      </Link>
       <button className="absolute top-2 right-2 text-2xl text-lightBeige text-shadow hover:text-darkGreen hover:scale-110 transform transition-transform duration-200">
         <FontAwesomeIcon icon={faHeart} />
       </button>
       <div className="p-4">
-        <a href="">
+        <Link to={`/venue/${venue.id}`}>
           <h2 className="text-xl font-black hover:underline hover:decoration-2 truncate">
-            {/* Room with a view that is absolutely stunning and breathtaking */}
             {venue.name}
           </h2>
-        </a>
+        </Link>
         <div className="text-sm flex w-full justify-between text-center mt-2 space-x-2">
           <div className="flex-grow font-bold border border-black rounded-sm p-1 inline-block">
-            $200/night
+            <span className="font-black">${venue.price}</span>/night
           </div>
           <div className="flex-grow border border-black rounded-sm p-1 inline-block">
-            <span className="font-bold">Guests:</span> 4
+            <span className="font-bold">Guests:</span> {venue.maxGuests}
           </div>
           <div className="flex-grow items-center border border-black rounded-sm p-1 inline-block">
             <FontAwesomeIcon icon={faStar} className="text-yellow-500" />
-            <span className="ml-1 font-bold">4.5</span>
+            <span className="ml-1 font-bold">{venue.rating}</span>
           </div>
         </div>
         <div className="flex w-full justify-between text-center mt-2 space-x-2">
-          <div className="text-sm font-bold flex-grow items-center border border-black rounded-sm p-1 inline-block">
+          <div className="text-sm font-bold flex-grow items-center border border-black rounded-sm p-1 inline-block truncate">
             <FontAwesomeIcon
               icon={faMapMarkerAlt}
               className="text-red-500 mr-2"
             />
-            Norway - Lofoten
+            {venue.location.country} - {venue.location.city}
           </div>
-          <div className="items-center space-x-2 flex-grow border border-black rounded-sm p-1 inline-block">
-            <span title="Wifi">
-              <FontAwesomeIcon icon={faWifi} />
+          <div className="items-center space-x-2 flex-grow border border-black rounded-sm p-1 inline-block min-w-[114px]">
+            <span title={venue.meta.wifi ? "Wifi available" : "No wifi"}>
+              <FontAwesomeIcon
+                icon={faWifi}
+                className={venue.meta.wifi ? "text-black" : "text-gray-400"}
+              />
             </span>
-            <span title="Pets Allowed">
-              <FontAwesomeIcon icon={faPaw} />
+            <span title={venue.meta.pets ? "Pets Allowed" : "No pets allowed"}>
+              <FontAwesomeIcon
+                icon={faPaw}
+                className={venue.meta.pets ? "text-black" : "text-gray-400"}
+              />
             </span>
-            <span title="Breakfast Included">
-              <FontAwesomeIcon icon={faUtensils} />
+            <span
+              title={
+                venue.meta.breakfast ? "Breakfast Included" : "No breakfast"
+              }
+            >
+              <FontAwesomeIcon
+                icon={faUtensils}
+                className={
+                  venue.meta.breakfast ? "text-black" : "text-gray-400"
+                }
+              />
             </span>
-            <span title="Parking Available">
-              <FontAwesomeIcon icon={faParking} />
+            <span
+              title={
+                venue.meta.parking
+                  ? "Parking Available"
+                  : "No parking available"
+              }
+            >
+              <FontAwesomeIcon
+                icon={faParking}
+                className={venue.meta.parking ? "text-black" : "text-gray-400"}
+              />
             </span>
           </div>
         </div>
-        <button className="w-full bg-lightGreen text-black font-bold py-2 px-4 rounded mt-4 shadow-custom-dark hover:bg-darkGreen">
-          View
-        </button>
+        <div className="w-full bg-lightGreen text-black font-bold py-2 px-4 rounded mt-4 shadow-custom-dark hover:bg-darkGreen">
+          <Link to={`/venue/${venue.id}`}>View</Link>
+        </div>
       </div>
     </div>
   );
