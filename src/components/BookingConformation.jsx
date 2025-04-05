e7127c4a60367838813fcd60c51e1b13f2bcea74
EliseAur <94222653+EliseAur@users.noreply.github.com>
2025-04-05 17:47:04 +0200

    Fix booking confirmation modal with link to account

--- src/components/BookingConformation.jsx
+++ src/components/BookingConformation.jsx
@@ -1,11 +1,21 @@
+import { Link } from "react-router-dom";
+
 export default function BookingConfirmation({ onClose }) {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-2xl font-bold mb-4">Booking Confirmation</h1>
-      <p className="text-lg mb-2">Thank you for your booking!</p>
+    <div className="flex flex-col items-center justify-center pb-10 pt-4">
+      <h1 className="text-lg sm:text-2xl font-black mb-2">
+        Booking Confirmation
+      </h1>
+      <p className="text-md sm:text-lg mb-2">Thank you for your booking!</p>
+      <Link
+        to="/account"
+        className=" text-black font-bold   hover:cursor-pointer underline hover:decoration-2 text-md sm:text-lg mb-4"
+      >
+        View all your bookings in account
+      </Link>
       <button
         onClick={onClose}
-        className="bg-blue-500 text-white px-4 py-2 rounded mt-4"
+        className="bg-lightGreen text-black font-bold py-2 px-4 rounded-sm hover:bg-darkGreen hover:cursor-pointer shadow-custom-dark"
       >
         Close
       </button>
