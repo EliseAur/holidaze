7baa574d502c5617aecaa8f305bb0786698b54ba
EliseAur <94222653+EliseAur@users.noreply.github.com>
2025-04-09 20:32:47 +0200

    Remove background styling in modal. Palms background is there, so it is not necessary.

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,10 +2,7 @@ export default function Modal({ isOpen, onClose, children }) {
   if (!isOpen) return null;
 
   return (
-    <div
-      className="modal fixed inset-0 z-100 overflow-auto flex justify-center items-center bg-cover bg-top bg-no-repeat"
-      style={{ backgroundColor: "rgba(0, 0, 0, 0.7)" }}
-    >
+    <div className="modal fixed inset-0 z-100 overflow-auto flex justify-center items-center bg-cover bg-top bg-no-repeat">
       <div className="bg-beige p-0 rounded-sm shadow-lg  mx-auto relative max-w-[500px] w-full">
         <button
           className="absolute top-3 right-7 text-black text-3xl font-bold"
