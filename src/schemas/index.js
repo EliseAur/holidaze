45374c7fbad8ab13ebd57c56893af2a3dc5456b1
EliseAur <94222653+EliseAur@users.noreply.github.com>
2025-04-26 17:10:10 +0200

    This belongs to the last commit

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,4 +1,6 @@
 import { VenueSchema } from "./VenueSchema";
 import { ProfileSchema } from "./ProfileSchema";
+import { LoginSchema } from "./LoginSchema";
+import { RegisterSchema } from "./RegisterSchema";
 
-export { VenueSchema, ProfileSchema };
+export { VenueSchema, ProfileSchema, LoginSchema, RegisterSchema };
